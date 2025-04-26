04af17ffb98f0aece808f3f64e9ca28ba4d87510
Alena Dounar <dr.dovn@gmail.com>
2025-04-26 23:45:34 +0200

    feat: implement cli block

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
+import process from 'process';
+
 const parseArgs = () => {
-    // Write your code here 
+    const args = process.argv.slice(2);
+
+    const result = args.reduce((line, arg, index, { length }) => {
+        const isLastArg = index === length - 1;
+        return line + (
+            arg.startsWith('--') 
+            ? `${arg.slice(2)} is `
+            : `${arg}${isLastArg ? '' : ', '}`
+        );
+    }, '');
+    
+    console.log(result);
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,8 @@
 const parseEnv = () => {
-    // Write your code here 
+    const result = Object.entries(process.env)
+        .reduce((line, [key, value]) => line + (key.startsWith('RSS_') ? `${key}=${value}; ` : ''), '');
+
+    console.log(result.slice(0, result.length - 2));
 };
 
 parseEnv();
