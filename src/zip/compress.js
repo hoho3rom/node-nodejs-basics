5d4930c2b755b74f03a12f60f0ff68b1adc78071
Alena Dounar <dr.dovn@gmail.com>
2025-04-28 17:46:30 +0200

    feat: implement zlib block

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import { createGzip } from 'zlib';
+
 const compress = async () => {
-    // Write your code here 
+    const gzip = createGzip();
+    const writeToFile = createWriteStream(`${import.meta.dirname}/files/archive.gz`);
+    const readFromFile = createReadStream(`${import.meta.dirname}/files/fileToCompress.txt`);
+
+    await pipeline(
+        readFromFile,
+        gzip,
+        writeToFile
+    );
 };
 
 await compress();
