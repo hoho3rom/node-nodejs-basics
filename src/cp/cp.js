caa831e6efba51a851d44e71e2304964bff8bbd9
Alena Dounar <dr.dovn@gmail.com>
2025-04-28 21:46:21 +0200

    feat: implement cp block

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,12 @@
+import { fork } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    fork(
+        `${import.meta.dirname}/files/script.js`, 
+        args, 
+        { stdio: ['inherit', 'inherit', 'inherit', 'ipc'] }
+    );
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['kek', "lol", "mem", 3])
