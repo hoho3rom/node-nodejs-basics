30c39c0b0e678a37becc015bf78c23e3a1c9bcce
Alena Dounar <dr.dovn@gmail.com>
2025-04-28 19:11:31 +0200

    feat: implement wt block

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,29 @@
+import { Worker } from 'worker_threads';
+import os from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+    const cores = os.cpus().length;
+    const results = [];
+    const promises = [];
+
+    for (let i = 0; i < cores; i++) {
+        promises.push(
+            runWorker(cores + i)
+                .then((result => results[i] = ({ status: 'resolved', data: result })))
+                .catch(() => results[i] = ({ status: 'error', data: null }))
+        );
+    }
+
+    await Promise.allSettled(promises).then(() => console.log(results));
 };
+
+const runWorker = (n) => {
+    return new Promise((resolve, reject) => {
+        const worker = new Worker(`${import.meta.dirname}/worker.js`, { workerData: { n } });
+
+        worker.on('message', resolve);
+        worker.on('error', reject);
+    })
+}
 
 await performCalculations();
